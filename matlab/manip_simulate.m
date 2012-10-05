ea383ebe07d86a10afd25ca6b343478554f6200c
Alex Burka <aburka@seas.upenn.edu>
2012-10-05 17:45:22 -0400

    down with R^n, long live SE(n)

--- matlab/manip_simulate.m
+++ matlab/manip_simulate.m
@@ -1,10 +1,10 @@
 % simulates the motion of featuers
 
-% inpust:
+% inputs:
 %   dims:      2 or 3, for 2D or 3D motion
 %   n:         number of features
 %   f:         number of frames
-%   T:         kinematic tree edges (feature 1 is always the root)
+%   S:         kinematic tree edges (feature 1 is always the root)
 %               e.g. if dims=2 and n=3
 %                 [ struct('links', [1 2], ...
 %                          'joint', 'prismatic', ...
@@ -20,90 +20,111 @@
 %                       (c=[1 1], r=0.75, min=0, max=pi], start=pi/4)
 
 % outputs:
-%   X:         motion over time, f x dims*n matrix
+%   X:         motion over time, f x n x dims+1 x dims+1 matrix (i.e. f x n array of homogeneous transformation matrices)
 
-function X = manip_simulate(dims, n, f, T)
-    X = zeros(f, n, dims);
-    X(1, 1, :) = unifrnd(-5, 5, dims, 1); % randomly initialize root
+function X = manip_simulate(dims, n, f, S)
+    X = zeros(f, n, dims+1, dims+1);
+    X(1,1, :,:) = T(unifrnd(-5, 5, [dims 1])) * R(unifrnd(-2*pi, 2*pi, [1*(dims==2)+3*(dims==3) 1])); % randomly initialize root
     
     % turn the joint types into function pointers
-    for j = 1:length(T)
-        T(j).joint = eval(['@forward_' T(j).joint]);
+    for j = 1:length(S)
+        S(j).joint = eval(['@forward_' S(j).joint]);
     end
     
     for frame = 1:f
         % put stuff in places
-        X(frame, :, :) = place_objects(squeeze(X(frame, :, :)), T, [1]);
+        X(frame,:, :,:) = place_objects(squeeze(X(frame,:, :,:)), S, 1);
         
         if frame < f
             % nudge parameters randomly
-            X(frame+1, 1, :) = X(frame, 1, :) ...  % randomly nudge root
-                               + 0*unifrnd(-.2, .2, [1, 1, dims]);
-            for j = 1:length(T)
-                bit = diff(T(j).bounds)/10;
-                T(j).state = T(j).state + unifrnd(-bit, bit);
-                T(j).state = max(min(T(j).state, ...
-                                     T(j).bounds(2,:)), ...
-                                 T(j).bounds(1,:));
+            dX = eye(dims+1);%T(unifrnd(-.1, .1, [dims 1])) * R(unifrnd(-.3, .3, [1*(dims==2)+3*(dims==3) 1]));
+            X(frame+1,1, :,:) = dX * squeeze(X(frame,1, :,:));  % randomly nudge root
+                
+            for j = 1:length(S)
+                bit = diff(S(j).bounds)/10;
+                S(j).state = S(j).state + unifrnd(-bit, bit);
+                S(j).state = max(min(S(j).state, ...
+                                     S(j).bounds(2,:)), ...
+                                 S(j).bounds(1,:));
             end
         end
     end
 end
 
-function X = place_objects(X, T, parent)
-    for joint = find(horzcat(T.a) == parent)
-        child = T(joint).b;
+% utility functions to generate rotations and translations
+function r = R(params)
+    if length(params) == 1
+        r = [cos(params(1)) -sin(params(1)) 0
+             sin(params(1))  cos(params(1)) 0
+             0               0              1];
+    elseif length(params) == 3
+        [a b c] = subsref(num2cell(params), substruct('{}', {':'})); % i.e. [a b c] = num2cell(params){:}
+        r = [cos(a)*cos(b) cos(a)*sin(b)*sin(c)-sin(a)*cos(c) cos(a)*sin(b)*cos(c)-sin(a)*sin(c) 0
+             sin(a)*cos(b) sin(a)*sin(b)*sin(c)+cos(a)*cos(c) sin(a)*sin(b)*cos(c)-cos(a)*sin(c) 0
+             -sin(b)       cos(b)*sin(c)                      cos(b)*cos(c)                      0
+             0             0                                  0                                  1];
+    else
+        error('I can only think in 2 or 3 dimensions');
+    end
+end
+
+function t = T(params)
+    if length(params) == 2
+        [x y] = subsref(num2cell(params), substruct('{}', {':'})); % i.e. [x y] = num2cell(params){:}
+        t = [1 0 x
+             0 1 y
+             0 0 1];
+    elseif length(params) == 3
+        [x y z] = subsref(num2cell(params), substruct('{}', {':'})); % i.e. [x y z] = num2cell(params){:}
+        t = [1 0 0 x
+             0 1 0 y
+             0 0 1 z
+             0 0 0 1];
+    else
+        error('I can only think in 2 or 3 dimensinos');
+    end
+end
+
+% helper function to do the forward kinematics
+% X here is just a single frame, i.e. n x dims+1 x dims+1
+function X = place_objects(X, S, parent)
+    for joint = find(horzcat(S.a) == parent)
+        child = S(joint).b;
         
         % place object
-        X(child, :) = T(joint).joint(X(parent, :), ...
-                                     T(joint).params, ...
-                                     T(joint).state);
+        X(child, :,:) = S(joint).joint(S(joint).params, S(joint).state) * squeeze(X(parent, :,:));
         
         % place children
-        X = place_objects(X, T, child);
+        X = place_objects(X, S, child);
     end
 end
 
 % in 2D and 3D
-%   params{1} is the offset from the from-object center to prismatic joint
-%   params{2] is the unit vector pointing in the direction of prismaticness
-%   state is the extension of the joint
-function x = forward_prismatic(from, params, state)
+%   params{1} SE(n) is the offset from the from-object center to prismatic joint
+%   params{2] R(n)  is the unit vector pointing in the direction of prismaticness
+%   state     R     is the extension of the joint
+function x = forward_prismatic(params, state) %#ok<DEFNU>
     offset = params{1};
     u = params{2};
     pos = state;
     
-    x = from + offset + u*pos;
-end
-
-% in 2D
-%   params{1} is the center of rotation (WRT from-object center)
-%   params{2} is the radius of the circle
-%   state is the angle around the circle
-% in 3D
-%   params{1} is the center of rotation (WRT from-object center)
-%   params{2} is the radius of the circle
-%   params{3} is the unit vector pointing out of the circle plane
-%   params{4} is the unit vector pointing along the radius at theta=0
-%   state is the angle around the circle
-function x = forward_revolute(from, params, state) %#ok<DEFNU>
-    center = params{1};
-    radius = params{2};
-    theta = state;
-    if length(params) == 4
-        u = params{3};
-        r = params{4};
-        tensor = @(a,b) reshape(kron(a,b), [3 3]);
-        x = from + center + (r*cos(theta) ...
-                             + cross(u, r)*sin(theta) ...
-                             + (tensor(u, u)*(1-cos(theta))*r')');
-    else
-        x = forward_prismatic(from, {center, [cos(theta) sin(theta)]}, radius);
-    end
+    x = T(u*pos) * offset;
 end
 
 % in 2D and 3D
-%   params{1} is the offset from the from-object center to the to-object center
-function x = forward_rigid(from, params, ~) %#ok<DEFNU>
-    x = forward_prismatic(from, {params{1}, zeros(size(params{1}))}, 0);
+%   params{1} SE(n) is the center of rotation (WRT from-object center)
+%   params{2} SE(n) is the offset from the center to the moving part at theta=0
+%   state     R     is the angle around the circle
+function x = forward_revolute(params, state) %#ok<DEFNU>
+    center = params{1};
+    radius = params{2};
+    theta = state;
+    
+    x = radius * R([theta 0 0]) * center;
+end
+
+% in 2D and 3D
+%   params{1} SE(n) is the offset from the from-object center to the to-object center
+function x = forward_rigid(params, ~) %#ok<DEFNU>
+    x = params{1};
 end
