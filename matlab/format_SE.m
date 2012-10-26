384eccb5bf01c10ae38f4282a81854c48982f371
Alex Burka <aburka@seas.upenn.edu>
2012-10-25 21:24:09 -0400

    fix format_SE in case sin(b)=0 (no thanks to wikipedia)

--- matlab/format_SE.m
+++ matlab/format_SE.m
@@ -5,14 +5,18 @@ function s = format_SE(se)
         t = se(1:2, 3)';
         
         % extract rotation
-        r = atan2(se(2,1), se(1,1));
+        r = atan2(se(2,1), se(1,1))*1;
     elseif dims == 3
         % extract translation
         t = se(1:3, 4)';
         
         % extract rotation
         % 3-1-3 Euler Angles http://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions#Conversion_formulae_between_formalisms
-        r = [-atan2(se(2,3), se(1,3)) acos(se(3,3)) atan2(se(3,2), se(3,1))];
+        if se(3,3) == 1 % i.e. sin(b) = 0
+            r = [0 0 atan2(se(1,2), se(1,1))];
+        else
+            r = [-atan2(se(2,3), se(1,3)) acos(se(3,3)) atan2(se(3,2), se(3,1))]*eye(3); % multiply by I to remove -0
+        end
     else
         error('I can only think in 2 or 3 dimensions.');
     end
