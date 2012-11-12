11ac03b6f01c2268b1c9eaccc8ff141acf560d23
Alex Burka <aburka@seas.upenn.edu>
2012-11-12 17:29:20 -0500

    typo in calculation of Dr

--- matlab/manip_learn.m
+++ matlab/manip_learn.m
@@ -25,7 +25,7 @@ function S = manip_learn(X, dbg)
     dbg(' Dr ');
     A = sym('a%d%d', [dims 1]); A = sym(A, 'real');
     B = sym('b%d%d', [dims 1]); B = sym(B, 'real');
-    s = evalc('simplify(jacobian(feval(symengine, ''norm'', A - B, 2), A))');
+    s = evalc('simplify(jacobian(feval(symengine, ''norm'', A - B, 2)^2, A))');
     Dr = eval(sprintf('@(ut, vt) %s', regexprep(subsref(regexp(s, '\n', 'split'), substruct('{}', {4})), {'a(\d)(\d)', 'b(\d)(\d)'}, {'ut($1,$2)', 'vt($1,$2)'})));
     dbg('done.\n');
     
@@ -128,59 +128,4 @@ function params = unpack_revolute(p, dims)
     t = p(i:i+dims-1); i = i+dims;
     r = p(i:i+n-1);    i = i+n;
     params{2} = T(t)*R(r);
-end
-
-function [err, grad] = jointfit(deltas, p, forward, inverse, unpack, Dq, Dr)
-    %fprintf('\t\t\t%s\n', format_SE(p{1}));
-    err = 0;
-    grad = zeros(size(p));
-    
-    % accumulate error at each frame
-    for frame = 1:length(deltas)
-        % perform IK to get the observed params
-        [state, Dki] = inverse(deltas{frame}, unpack(p));
-        
-        % get error between real SE pose and observed-modeled SE pose
-        [estimate, Dkr, Dkt] = forward(unpack(p), state);
-        [e, Dd] = SE_dist(estimate, deltas{frame}, Dkr, Dkt, Dki, Dq, Dr);
-        err = err + e;
-        grad = grad + Dd;
-    end
-end
-
-% returns "distance" between two elements of SE(n)
-% also returns gradient of the distance WRT u
-function [dist, grad] = SE_dist(u, v, Dkr, Dkt, Dki, Dq, Dr)
-    % magic scale factors (see, e.g. Park 1995)
-    c = 2;
-    d = 1;
-
-    n = size(u,1)-1; % n as in SE(n)
-    
-    ut = u(1:n, n+1);
-    vt = v(1:n, n+1);
-    ur = u(1:n, 1:n);
-    vr = v(1:n, 1:n);
-    
-    %dr = ur\vr;
-    dt = ut - vt;
-    %L = logm_so3(dr);
-    %C = c*norm(L)^2;
-    if n == 3
-        tr = (ur(1,1)*ur(2,2)*vr(3,3) - ur(1,1)*ur(2,3)*vr(3,2) - ur(1,1)*ur(3,2)*vr(2,3) + ur(1,1)*ur(3,3)*vr(2,2) - ur(1,2)*ur(2,1)*vr(3,3) + ur(1,2)*ur(2,3)*vr(3,1) + ur(1,2)*ur(3,1)*vr(2,3) - ur(1,2)*ur(3,3)*vr(2,1) + ur(1,3)*ur(2,1)*vr(3,2) - ur(1,3)*ur(2,2)*vr(3,1) - ur(1,3)*ur(3,1)*vr(2,2) + ur(1,3)*ur(3,2)*vr(2,1) + ur(2,1)*ur(3,2)*vr(1,3) - ur(2,1)*ur(3,3)*vr(1,2) - ur(2,2)*ur(3,1)*vr(1,3) + ur(2,2)*ur(3,3)*vr(1,1) + ur(2,3)*ur(3,1)*vr(1,2) - ur(2,3)*ur(3,2)*vr(1,1))/(ur(1,1)*ur(2,2)*ur(3,3) - ur(1,1)*ur(2,3)*ur(3,2) - ur(1,2)*ur(2,1)*ur(3,3) + ur(1,2)*ur(2,3)*ur(3,1) + ur(1,3)*ur(2,1)*ur(3,2) - ur(1,3)*ur(2,2)*ur(3,1));
-    else
-        tr = (ur(1,1)*vr(2,2) - ur(1,2)*vr(2,1) - ur(2,1)*vr(1,2) + ur(2,2)*vr(1,1))/(ur(1,1)*ur(2,2) - ur(1,2)*ur(2,1));
-    end
-    C = c*2*acos((tr-1)/2); % from http://en.wikipedia.org/wiki/Axis-angle_representation#Log_map_from_SO.283.29_to_so.283.29
-    D = d*norm(dt)^2;
-    dist = C + D;
-
-    if nargout == 2
-        if nargin ~= 7
-            error('To calculate a gradient SE_dist needs gradient inputs');
-        else
-            %fprintf('SIZES: Dq[%d %d] Dr[%d %d] Dkr[%d %d] Dkt[%d %d] Dki[%d %d]\n', size(Dq(ur,vr),1),size(Dq(ur,vr),2), size(Dr(ut,vt),1),size(Dr(ut,vt),2), size(Dkr,1),size(Dkt,2), size(Dkt,1),size(Dkt,2), size(Dki,1),size(Dki,2));
-            grad = c*Dq(ur,vr)*Dkr*Dki + d*Dr(ut,vt)*Dkt*Dki;
-        end
-    end
 end
