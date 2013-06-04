fd3e8da6ac0654a730a4c8a9bd92ef21990f07a2
Alex Burka <aburka@seas.upenn.edu>
2013-06-04 13:07:04 -0400

    probably non-working guess_screw

--- matlab/guess_screw.m
+++ matlab/guess_screw.m
@@ -1,5 +1,12 @@
 function params = guess_screw(deltas, t1, r1, t2, r2, t3, r3)
 
-    params = [guess_revolute(deltas, t1, r1, t2, r2, t3, r3) 1];
+    Y = permute(cat(3, deltas{:}), [3 1 2]);
+    Y = squeeze(Y(:,1:3,4));
+    [a, r, p, plane] = helfit2(Y);
+    
+    c = [plane(4:6)' plane(7:9)' a];
+    [ct, cr] = extract_SE(R(c));
+    
+    params = [ct, cr, r 0 0, 0 0 0, p];
     
 end
