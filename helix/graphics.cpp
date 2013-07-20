0ef68ad427e0bcdceff65a8890754e98fd3e5f25
Alex Burka <aburka@seas.upenn.edu>
2013-07-20 14:30:54 -0400

    calculate and show FPS

--- helix/graphics.cpp
+++ helix/graphics.cpp
@@ -19,11 +19,26 @@ namespace acquire
 
     bool Graphics::loop(bool lameduck)
     {
+
         if (lameduck) return false;
 
         CookedPacket pkt;
         q.wait_and_pop(pkt);
 
+        // TODO
+        // - scale down and show both images
+        // - support "scroll lock"
+
+        static double ticks = getTickCount(), fps = 0;
+        if (pkt.index % 10 == 0) {
+            fps = 10.0 / ( ((double)getTickCount() - ticks) / (double)getTickFrequency() );
+            ticks = getTickCount();
+        }
+
+        ostringstream fps_str;
+        fps_str << "FPS: " << fps;
+        putText(pkt.dirty, fps_str.str(), cvPoint(50, 50), FONT_HERSHEY_PLAIN, 1, cvScalar(255,0,0));
+        
         imshow(prefix, pkt.dirty);
         waitKey(10);
 
